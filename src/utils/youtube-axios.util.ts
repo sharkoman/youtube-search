a9fc6d8a19fe5feca91f490e4d147eb6c5c0c14f
sharkoman <sharko.designer@gmail.com>
2022-10-22 22:53:02 +0200

    fix: :art: increase max results to 4

--- src/utils/youtube-axios.util.ts
+++ src/utils/youtube-axios.util.ts
@@ -4,7 +4,7 @@ export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
 	params: {
 		part: 'snippet',
-		maxResults: 2,
+		maxResults: 4,
 		key: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
 	},
 });
