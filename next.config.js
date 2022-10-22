0d3e3ecf4a4c03aefba2f4465f1dc941c51b0b2a
sharkoman <sharko.designer@gmail.com>
2022-10-22 22:53:34 +0200

    feat: :art: update next config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const path = require('path');
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	images: {
-		domains: ['i.ytimg.com'],
-	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, '/src', 'styles')],
 	},
